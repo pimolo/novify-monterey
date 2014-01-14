0560b2c526880ed11d0b345d7356455732fb291a
Rémi Andrieux <pimolo10300@gmail.com>
2014-01-14 16:54:02 +0100

    form ok

--- script.js
+++ script.js
@@ -45,28 +45,41 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    $('#ticket #ticketimg span').click(function() {
+        if ($('#ticket form').css("margin-top") >= "300px") {
+            $('#ticket form').css("margin-top", "100px");
+        };
+
+        if ($(this).parent().css("margin-bottom") === "300px") {
+            $(this).parent().css("margin-bottom", "0");
+        };
+
+        $('#ticket form').animate({"margin-top": "+=300"}, "fast");
+        $(this).parent().animate({"margin-bottom": "+=300px"}, "fast");
+    });
+// voir pour conditionner plutôt ces 2 dernières lignes, cela éviterait de faire descendre à chaque fois
     $('#ticket #ticketimg span.a').click(function() {
-        $('form #temps').append('1 DAY');
-        $('form #prix').append('50$');
+        $('#ticket form #temps').html('1 DAY');
+        $('#ticket form #prix').html('50$');
     });
 
     $('#ticket #ticketimg span.b').click(function() {
-        $('form #temps').append('2 DAY');
-        $('form #prix').append('80$');
+        $('#ticket form #temps').html('2 DAY');
+        $('#ticket form #prix').html('80$');
     });
 
     $('#ticket #ticketimg span.c').click(function() {
-        $('form #temps').append('1 DAY');
-        $('form #prix').append('42$');
+        $('#ticket form #temps').html('1 DAY');
+        $('#ticket form #prix').html('42$');
     });
 
     $('#ticket #ticketimg span.d').click(function() {
-        $('form #temps').append('2 DAY');
-        $('form #prix').append('90$');
+        $('#ticket form #temps').html('2 DAY');
+        $('#ticket form #prix').html('90$');
     });
 
     $('#ticket #ticketimg span.e').click(function() {
-        $('form #temps').append('1 DAY');
-        $('form #prix').append('100$');
+        $('#ticket form #temps').html('1 DAY');
+        $('#ticket form #prix').html('100$');
     });
 });
